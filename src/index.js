164f2d0ac1b4f75cb8ddb6fa72b735ac2934eabf
Victorv <v.vogt@etu.unistra.fr>
2021-11-17 11:45:19 +0100

    :construction: Col Component

--- src/index.js
+++ src/index.js
@@ -58,31 +58,69 @@ function ButtonPanel({ position, rotation }) {
   )
 }
 
+function DataCol({firstcol, position, colSize, cellSize, rotation}){
+  const [hover, setHover] = useState(false)
+  const [color, setColor] = useState(0x123456)
+
+  const onSelect = () => {
+    if (color==0xffffff)
+      setColor(0xffa36e);
+    else
+      setColor(0xffffff);
+  }
+
+  const onHover = () => {
+    setColor(0xffa36e);
+    setHover(true);
+  }
+
+  const onBlur = () => {
+    setColor(0xffffff);
+    setHover(false);
+  }
+  
+  const generateCells = () => {
+    const row = [];
+    for (let i=0; i < colSize; i++){
+      let size = [cellSize[0], cellSize[1], 0.1];
+      let position = [0, cellSize[1]*(colSize/2-i), 0.1];
+      let text = colSize+'x'+i;
+      let colorBtn=color;
+      let fontColor=0x000000
+      if(firstcol){
+        text = i+'';
+        colorBtn=0x000000;
+        fontColor=0xffffff;
+      } else if (i==0) {
+        text = 'Col'+ i;
+        colorBtn=0x000000;
+        fontColor=0xffffff;
+      }
+      row.push(<Button key={i+''+colSize} position={position} fontSize={0.1} fontColor={fontColor} color={colorBtn} size={size}>{text}</Button>);
+    }
+    return row;
+  }
+
+  return (
+    <Interactive onSelect={onSelect} onHover={onHover} onBlur={onBlur}>
+      <Box position={position} rotation={rotation} size={[cellSize[0], cellSize[1]*colSize, 0.01]}>
+        {generateCells()}
+      </Box>
+    </Interactive>
+  )
+}
+
 function SpreadSheet({position, gridSize, cellSize, anglemax}){
   const generateGrid = ([x, y, z]) => {
     const row = [];
     let startX = position[0]-gridSize[0]/2*cellSize[0];
     let startY = position[1]+gridSize[1]/2*cellSize[1];
     for (let i=0; i < gridSize[0]; i++){
-      for (let j=0; j < gridSize[1]; j++){
-        let mirrorX = i-gridSize[0]/2;
-        let rotation = [0, mirrorX/(gridSize[0]/2)*anglemax, 0];
-        let position = [startX+i*cellSize[0], startY-j*cellSize[1], z+0.025*mirrorX*mirrorX];
-        let size = [cellSize[0]+0.0025*mirrorX*mirrorX, cellSize[1], 0.1];
-        let text = i+'x'+j;
-        let color=0xffffff;
-        let fontColor=0x000000
-        if(i==0){
-          text = j+'';
-          color=0x000000;
-          fontColor=0xffffff;
-        } else if (j==0) {
-          text = 'Col'+ i;
-          color=0x000000;
-          fontColor=0xffffff;
-        }
-        row.push(<Button key={i+''+j} fontSize={0.1} fontColor={fontColor} color={color} size={size} position={position} rotation={rotation}>{text}</Button>);
-      }
+      let mirrorX = i-gridSize[0]/2;
+      let rotation = [0, mirrorX/(gridSize[0]/2)*anglemax, 0];
+      let position = [startX+i*cellSize[0], startY, z+0.025*mirrorX*mirrorX];
+      let size = [cellSize[0]+0.0025*mirrorX*mirrorX, cellSize[1], 0.1];
+      row.push(<DataCol firstcol={false} position={position} rotation={rotation} colSize={gridSize[1]} cellSize={size} />);
     }
     return row;
   }
