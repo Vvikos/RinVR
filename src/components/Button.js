e80d444b5eef2bd50bed87d2f5fe7bb5e0375576
Victorv <v.vogt@etu.unistra.fr>
2021-12-09 21:59:15 +0100

    :sparkles: set up website name

--- src/components/Button.js
+++ src/components/Button.js
@@ -5,7 +5,7 @@ import Box from './Box'
 function Button({ children, size, color, fontSize, fontColor, ...rest}) {
   return (
     <Box color={color} size={size} {...rest}>
-      <Text fontSize={fontSize} position={[0,0,0.06]} maxWidth={size[0]-0.2} color={fontColor} anchorX="left" anchorY="middle">
+      <Text fontSize={fontSize} position={[0,0,0.06]} maxWidth={size[0]-0.2} color={fontColor} anchorX="middle" anchorY="middle">
         {children}
       </Text>
     </Box>
--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -29,21 +29,21 @@ function ButtonPanel({ position, rotation, onDropDownChangeValue, dropDownValue,
       <Box color={color} size={[1, 1, 0.01]} position={position} rotation={rotation}>
         <DropDown color={0xffffff} onChangeValue={onDropDownChangeValue} dropDownValue={dropDownValue} position={[-0.25, -0.25, 0.02]}/>
         <Interactive onSelect={function() { onSelect(); onClickRowNext(); }} onHover={onHover} onBlur={onBlur}>
-          <Button color={0xfc2617} fontColor={0xffffff} fontSize={0.015} size={[0.15, 0.1, 0.04]} position={[0.2, -0.15, 0.03]}>Up</Button>
+          <Button color={0xfc2617} fontColor={0xffffff} fontSize={0.03} size={[0.15, 0.1, 0.02]} position={[0.2, -0.15, 0.03]}>Up</Button>
         </Interactive>
         <Interactive onSelect={function() { onSelect(); onClickRowPrev(); }} onHover={onHover} onBlur={onBlur}>
-          <Button color={0xfc2617} fontColor={0xffffff} fontSize={0.015} size={[0.15, 0.1, 0.04]} position={[0.2, 0.15, 0.03]}>Down</Button>
+          <Button color={0xfc2617} fontColor={0xffffff} fontSize={0.03} size={[0.15, 0.1, 0.02]} position={[0.2, 0.15, 0.03]}>Down</Button>
         </Interactive>
   
         <Interactive onSelect={function() { onSelect(); onClickRefresh(); }} onHover={onHover} onBlur={onBlur}>
-          <Button color={0x16a5f2} fontColor={0xffffff} fontSize={0.015} size={[0.2, 0.12, 0.08]} position={[-0.2, 0, 0.03]}>Refresh</Button>
+          <Button color={0x16a5f2} fontColor={0xffffff} fontSize={0.03} size={[0.2, 0.12, 0.02]} position={[-0.2, 0, 0.03]}>Refresh</Button>
         </Interactive>
         
         <Interactive onSelect={function() { onSelect(); onClickColNext(); }} onHover={onHover} onBlur={onBlur}>
-          <Button color={0xfc2617} fontColor={0xffffff} fontSize={0.015} size={[0.15, 0.1, 0.04]} position={[0.35, 0, 0.03]}>Right</Button>
+          <Button color={0xfc2617} fontColor={0xffffff} fontSize={0.03} size={[0.15, 0.1, 0.02]} position={[0.35, 0, 0.03]}>Right</Button>
         </Interactive>
         <Interactive onSelect={function() { onSelect(); onClickColPrev(); }} onHover={onHover} onBlur={onBlur}>
-          <Button color={0xfc2617} fontColor={0xffffff} fontSize={0.015} size={[0.15, 0.1, 0.04]} position={[0.05, 0, 0.03]}>Left</Button>
+          <Button color={0xfc2617} fontColor={0xffffff} fontSize={0.03} size={[0.15, 0.1, 0.02]} position={[0.05, 0, 0.03]}>Left</Button>
         </Interactive>
       </Box>
     )
